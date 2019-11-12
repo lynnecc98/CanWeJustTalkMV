3c46cc1dca4b455863af6ddd9d071996cb595d21
Brian Kalish <bkalish42@gmail.com>
2019-11-12 17:32:54 -0500

    Primary Zoom In Function Implemented
    We can zoom in and out now. Though it's not hooked up to anything and the text can't display yet.

---
+++ Assets/Scripts/Zoom.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zoom : MonoBehaviour
+{
+    public float outLoc;
+    public float inLoc;
+    public float zoomSpd;
+
+    public bool zoomed;
+    private Transform t;
+    private Rigidbody rb;
+    private Canvas c;
+    // Start is called before the first frame update
+    void Start()
+    {
+        zoomed = false;
+        t = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody>();
+        c = FindObjectOfType<Canvas>();
+        c.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (zoomed == true && t.localPosition.z < inLoc)
+        {
+            rb.velocity = Vector3.forward * zoomSpd;
+        }
+        else if (zoomed == false && t.localPosition.z > outLoc)
+        {
+            rb.velocity = Vector3.back * zoomSpd;
+        }
+        else
+        {
+            rb.velocity = Vector3.zero;
+        }
+        if (t.localPosition.z < outLoc)
+        {
+            t.localPosition = new Vector3(t.localPosition.x, t.localPosition.y, outLoc);
+        }
+        else if (t.localPosition.z > inLoc)
+        {
+            t.localPosition = new Vector3(t.localPosition.x, t.localPosition.y, inLoc);
+        }
+    }
+}
